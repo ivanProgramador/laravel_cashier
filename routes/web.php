8df2711abdda325e0e21de33d6ec56708e599691
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-11-03 21:38:22 -0300

    view de teste

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-
-  $users = User::all();
-  
-  dd($users);
-});
+Route::get('/login',[MainController::class,'loginPage']);
