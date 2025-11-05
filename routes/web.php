1e17f3833b038aa614d58df00c4b2c9a065cdeeb
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-11-05 01:05:05 -0300

    controle de acesso

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,25 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\isGuest;
+use App\Http\Middleware\isUser;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login',[MainController::class,'loginPage'])->name('login');
-Route::get('/login/{id}',[MainController::class,'loginSubmit'])->name('login.submit');
-Route::get('/logout',[MainController::class,'logout'])->name('logout');
-Route::get('/planos',[MainController::class,'planos'])->name('planos');
+//rotas pra visitantes 
+Route::middleware([isGuest::class])->group(function(){
+    Route::get('/login',[MainController::class,'loginPage'])->name('login');
+    Route::get('/login/{id}',[MainController::class,'loginSubmit'])->name('login.submit');
+});
+
+//rotas pra usuarios 
+Route::middleware([isUser::class])->group(function(){
+    Route::redirect('/','login');
+    Route::get('/logout',[MainController::class,'logout'])->name('logout');
+    Route::get('/planos',[MainController::class,'planos'])->name('planos');
+});
+
+
+
+
 
